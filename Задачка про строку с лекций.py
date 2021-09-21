0230fe1777c85bb516218bf5177e31ee48c7055c
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-09-21 15:47:05 +0500

    Create Задачка про строку с лекций.py
    Поиск наибольшего кол-ва цифр

---
+++ Задачка про строку с лекций.py
@@ -0,0 +1,10 @@
+print("Введите строку: ")
+s = input()
+l = []
+for i in range(0, 10):
+    l.append(s.count(str(i)))
+print("Наибольшее количество цифр:", max(l))
+print("Это цифра(ы):")
+for n in range(0, 10):
+    if l[n] == (max(l)):
+        print(n)
