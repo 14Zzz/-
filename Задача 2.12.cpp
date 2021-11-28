d0192c7977a70a278bf368cacc8bd90cbece5103
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-11-28 23:40:15 +0500

    Update Задача 2.12.cpp

--- Задача 2.12.cpp
+++ Задача 2.12.cpp
@@ -3,23 +3,24 @@ using namespace std;
 
 int main()
 {
- int n, i,max, j, min;
- cout << "Введите количество элементов массива: ";
- cin >> n;
- cout << "Последовательно введите элементы массива: ";
- cin >> j;
- max = min = j;
- for (i = n - 1; i > 0; i--)
- {
-  cin >> j;
-  if (j > max)
-  {
-   max = j;
-  }
-  if (j < min)
-  {
-   min = j;
-  }
- }
- cout << "Сумма min и max = " << max + min;
+	setlocale(LC_ALL, "RUS");
+	int n, i, max, j, min;
+	cout << "Введите количество элементов массива: ";
+	cin >> n;
+	cout << "Последовательно введите элементы массива: ";
+	cin >> j;
+	max = min = j;
+	for (i = n - 1; i > 0; i--)
+	{
+		cin >> j;
+		if (j > max)
+		{
+			max = j;
+		}
+		if (j < min)
+		{
+			min = j;
+		}
+	}
+	cout << "Сумма min и max = " << max + min;
 }
