6b41283074bbeba791cd5d7891521da26706805b
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-11-28 23:42:55 +0500

    Update Задача 2.13.cpp

--- Задача 2.13.cpp
+++ Задача 2.13.cpp
@@ -3,23 +3,24 @@
 using namespace std;
 int main()
 {
- double D, x, x1, x2, a, b, c;
- cout « "Введите коэфиценты a, b и c: ";
- cin » a » b » c;
- D = pow(b,2)- 4 * a * c;
- if (D == 0)
- {
-  cout « "Корней нет"; 
- }
- else if (D > 0)
- {
-  x1 = (-b - sqrt(D)) / (2 * a);
-  x2 = (-b + sqrt(D)) / (2 * a);
-  cout « "x1 = " « x1 « " x2 = " « x2;
- }
- else
- {
-  x = -b / (2 * a);
-  cout « "x = " « x;
- }
+	setlocale(LC_ALL, "RUS");
+	double D, x, x1, x2, a, b, c;
+	cout << "Введите коэфиценты a, b и c: ";
+	cin >> a >> b >> c;
+	D = pow(b, 2) - 4 * a * c;
+	if (D == 0)
+	{
+		cout << "Корней нет";
+	}
+	else if (D > 0)
+	{
+		x1 = (-b - sqrt(D)) / (2 * a);
+		x2 = (-b + sqrt(D)) / (2 * a);
+		cout << "x1 = " << x1 << " x2 = " << x2;
+	}
+	else
+	{
+		x = -b / (2 * a);
+		cout << "x = " << x;
+	}
 }
