5f9b528678fc3257108225dc18e5eaab04126aa3
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-11-28 23:45:44 +0500

    Update Задача 2.15.cpp

--- Задача 2.15.cpp
+++ Задача 2.15.cpp
@@ -3,28 +3,29 @@ using namespace std;
 
 int main()
 {
- int s, z, x;
- bool Vhodit = false;
- cout « "Введите число: ";
- cin » x;
- cout « "Введите символ: ";
- cin » z;
- while (x > 0)
- {
-  s = x % 10;
-  x = x / 10;
-  if (s == z)
-  {
-   Vhodit = true;
-   break;
-  }
- }
- if (Vhodit)
- {
-  cout « "Входит";
- }
- else
- {
-  cout « "Не входит";
- }
+	setlocale(LC_ALL, "RUS");
+	int s, z, x;
+	bool Vhodit = false;
+	cout << "Введите число: ";
+	cin >> x;
+	cout << "Введите символ: ";
+	cin >> z;
+	while (x > 0)
+	{
+		s = x % 10;
+		x = x / 10;
+		if (s == z)
+		{
+			Vhodit = true;
+			break;
+		}
+	}
+	if (Vhodit)
+	{
+		cout << "Входит";
+	}
+	else
+	{
+		cout << "Не входит";
+	}
 }
