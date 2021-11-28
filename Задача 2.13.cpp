6b037173b8a8b83f976a510701cec7bb7dd6ce96
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-11-29 00:29:44 +0500

    Update Задача 2.13.cpp

--- Задача 2.13.cpp
+++ Задача 2.13.cpp
@@ -12,13 +12,13 @@ int main()
 	{
 		cout << "Корней нет";
 	}
-	else if (D > 0)
+	if (D > 0)
 	{
 		x1 = (-b - sqrt(D)) / (2 * a);
 		x2 = (-b + sqrt(D)) / (2 * a);
 		cout << "x1 = " << x1 << " x2 = " << x2;
 	}
-	else
+	if (D < 0)
 	{
 		x = -b / (2 * a);
 		cout << "x = " << x;
