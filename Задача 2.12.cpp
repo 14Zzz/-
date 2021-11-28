64c7357043572397a80e8122614a33a36b5160c6
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-11-28 23:32:31 +0500

    Create Задача 2.12.cpp

---
+++ Задача 2.12.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int n, i,max, j, min;
+ cout « "Введите количество элементов массива: ";
+ cin » n;
+ cout « "Последовательно введите элементы массива: ";
+ cin » j;
+ max = min = j;
+ for (i = n - 1; i > 0; i--)
+ {
+  cin » j;
+  if (j > max)
+  {
+   max = j;
+  }
+  if (j < min)
+  {
+   min = j;
+  }
+ }
+ cout « "Сумма min и max = "« max + min;
+}
