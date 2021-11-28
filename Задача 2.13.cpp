4837ab37e0b3d439fc8a4729e708483988e153d4
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-11-28 23:32:53 +0500

    Create Задача 2.13.cpp

---
+++ Задача 2.13.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+ double D, x, x1, x2, a, b, c;
+ cout « "Введите коэфиценты a, b и c: ";
+ cin » a » b » c;
+ D = pow(b,2)- 4 * a * c;
+ if (D == 0)
+ {
+  cout « "Корней нет"; 
+ }
+ else if (D > 0)
+ {
+  x1 = (-b - sqrt(D)) / (2 * a);
+  x2 = (-b + sqrt(D)) / (2 * a);
+  cout « "x1 = " « x1 « " x2 = " « x2;
+ }
+ else
+ {
+  x = -b / (2 * a);
+  cout « "x = " « x;
+ }
+}
