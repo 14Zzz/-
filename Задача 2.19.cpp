40d5f58ecce91b592566e295e4de1cacf7bf4dab
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-12-05 19:17:21 +0500

    Create Задача 2.19.cpp

---
+++ Задача 2.19.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n;
+	int sum = 0;
+	cout << "Длинну послендовательности: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) 
+	{
+		if (i % 3 == 0) {
+			sum -= i;
+		}
+		else {
+			sum += i;
+		}
+	}
+	cout << "Сумма последовательности: " << sum << endl;
+	return 0;
+}
