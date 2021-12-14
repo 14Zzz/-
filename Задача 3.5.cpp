adc4f5472220a7c03504d35d3645eb6ca1cef205
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-12-14 21:20:36 +0500

    Update Задача 3.5.cpp

--- Задача 3.5.cpp
+++ Задача 3.5.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "rus");
     int a[7], count = 1, max;
+    cout << "Введите 7 элементов массива: ";
     for (int i = 0; i < 7; i++)
     {
-        cout << "Введите 7 элементов массива: ";
         cin >> a[i];
     }
     max = a[0];
-    for (int i = 0; i < 7; i++) 
+    for (int i = 0; i < 7; i++)
     {
-        if (a[i] > max) 
+        if (a[i] > max)
         {
             max = a[i];
             count = 1;
         }
-        else if (a[i] == max) 
+        else if (a[i] == max)
         {
             count++;
         }
