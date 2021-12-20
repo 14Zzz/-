5356fba461b9bb67d8c7e76070201beafc3babfa
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-12-20 23:21:09 +0500

    Create Задача 3.3.cpp

---
+++ Задача 3.3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int j, p, i, a[5], min, tmp;
+	cout << "Введите 5 элементов массива: ";
+	for (i = 0; i < 5; i++)
+	{
+		cin >> tmp;
+		a[i] = tmp;
+	}
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5 - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+	for (i = 0; i < 5; i++)
+	{
+		cout << a[i];
+	}
+}
