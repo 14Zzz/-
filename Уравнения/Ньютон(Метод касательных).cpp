17eccc44d32652fc3c61f08aafc3b408ad47c398
Maxim Burylov <91120648+14Zzz@users.noreply.github.com>
2021-12-20 21:43:59 +0500

    Update Ньютон(Метод касательных).cpp

--- Уравнения/Ньютон(Метод касательных).cpp
+++ Уравнения/Ньютон(Метод касательных).cpp
@@ -7,12 +7,6 @@ int main()
 	setlocale(LC_ALL, "rus");
 	float x, xPrev;
 	double eps = 0.000001;
-	// f(x) = x ^ 3 -x -1
-	// f'(x) = 3 * x ^ 2 - 1
-	// f''(x) = 6 * x
-	// [1, 2]
-	// f(b) * f"(b) > 0, если а > 0, то так же с а
-	// 8 - 2 - 1 * 6 * 2 > 0, х0 = а
 	x = 2;
 	xPrev = 0;
 	while (abs(x - xPrev) >= eps)
